04bdd455d1ec6397ea2530dcfa1377c277d1bfdb
akrish44 <akrish44@aol.com>
2013-09-09 19:39:32 -0600

    Update Hailstone.java

--- Hailstone.java
+++ Hailstone.java
@@ -8,18 +8,18 @@ public class Hailstone {
 		System.out.println(str1);
 		int newNum = reader.nextInt();
 
-	while (newNum > 0 && newNum !=1) {
-		while (newNum % 2 == 0) {
-			newNum = newNum/2;
-			System.out.println(newNum);
-		}
-		
-		while (newNum != 1 && newNum % 2 != 0) {
-			newNum = ((3*newNum) + 1);
-			System.out.println(newNum);
-		}	
-		if (newNum == 1) {
-			System.out.println("Calculation stopped.");
+		while (newNum > 0 && newNum !=1) {
+			while (newNum % 2 == 0) {
+				newNum = newNum/2;
+				System.out.println(newNum);
+			}
+			
+			while (newNum != 1 && newNum % 2 != 0) {
+				newNum = ((3*newNum) + 1);
+				System.out.println(newNum);
+			}	
+			if (newNum == 1) {
+				System.out.println("Calculation stopped.");
 		}
 	}
 }
